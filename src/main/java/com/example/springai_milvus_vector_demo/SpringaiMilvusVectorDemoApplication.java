a7c885ec14d896a9e84ccc33a65f4adf60b7fb98
Vinay Dadaso Hinukale <jtr529@york.ac.uk>
2024-10-03 18:47:59 +0100

    This project demonstrates the integration of Milvus, a vector database, with Spring Boot to build a vector store service. The application provides endpoints for loading documents into the Milvus vector store and searching for similar documents using vector embeddings.

---
+++ src/main/java/com/example/springai_milvus_vector_demo/SpringaiMilvusVectorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springai_milvus_vector_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiMilvusVectorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiMilvusVectorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springai_milvus_vector_demo/SpringaiMilvusVectorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springai_milvus_vector_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiMilvusVectorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
